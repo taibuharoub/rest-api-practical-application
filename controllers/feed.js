52fddfa6f25e1ab205903ac9df54cb9bf68c0711
Taibu Haroub <haroubtaibu@gmail.com>
2021-05-25 19:07:42 +0300

    Add logic to update a post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/Post");
@@ -33,7 +36,7 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  const imageUrl = req.file.path.replace("\\" ,"/");
+  const imageUrl = req.file.path.replace("\\", "/");
   const { title, content } = req.body;
   console.log(title, content);
 
@@ -78,3 +81,52 @@ exports.getPost = (req, res, next) => {
       next(err);
     });
 };
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const { title, content } = req.body;
+  let imageUrl = req.body.image; //no new file is picked
+  if (req.file) {
+    imageUrl = req.file.path; //new file picked
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked.");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post updated!", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, err => console.log(err));
+}
